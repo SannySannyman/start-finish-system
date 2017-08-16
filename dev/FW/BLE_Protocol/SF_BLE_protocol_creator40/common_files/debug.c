ed6c011c2ac7db459ba499062edc947ba29373d3
Oleksandr Podolchak <ol.podolchak@gmail.com>
2017-08-16 22:27:12 +0300

    виправлено порядок надсилання символів закінчення рядка (замінено на \r\n)

--- dev/FW/BLE_Protocol/SF_BLE_protocol_creator40/common_files/debug.c
+++ dev/FW/BLE_Protocol/SF_BLE_protocol_creator40/common_files/debug.c
@@ -71,7 +71,7 @@ void DEBUG_BLE_PrintApiResult(CYBLE_API_RESULT_T apiResult)
             break;
             
         case CYBLE_ERROR_INVALID_STATE:
-            DBG_PRINTF("invalid state: ");
+            DBG_PRINTF("invalid state\r\n");
             break;
             
         case CYBLE_ERROR_GATT_DB_INVALID_ATTR_HANDLE:
@@ -142,17 +142,17 @@ void DEBUG_BLE_PrintAppEvent(uint32 event, void* eventParam)
     switch(event)
     {
         case CYBLE_EVT_STACK_ON:
-            DBG_PRINTF("CYBLE_EVT_STACK_ON \n\r");
+            DBG_PRINTF("CYBLE_EVT_STACK_ON \r\n");
             break;
 
         case CYBLE_EVT_TIMEOUT: /* 0x01 -> GAP limited discoverable mode timeout. */
                                 /* 0x02 -> GAP pairing process timeout. */
                                 /* 0x03 -> GATT response timeout. */
-            DBG_PRINTF("CYBLE_EVT_TIMEOUT: %d \n\r", *(uint8 *)eventParam);
+            DBG_PRINTF("CYBLE_EVT_TIMEOUT: %d \r\n", *(uint8 *)eventParam);
             break;
 
         case CYBLE_EVT_HCI_STATUS:
-            DBG_PRINTF("CYBLE_EVT_HCI_STATUS: %x\n\r", *(uint8 *)eventParam);
+            DBG_PRINTF("CYBLE_EVT_HCI_STATUS: %x\r\n", *(uint8 *)eventParam);
             break;
         /**********************************************************
         *                       GAP Events
@@ -163,28 +163,28 @@ void DEBUG_BLE_PrintAppEvent(uint32 event, void* eventParam)
             break;    
             
         case CYBLE_EVT_GAPC_SCAN_START_STOP:
-            DBG_PRINTF("CYBLE_EVT_GAPC_SCAN_START_STOP \n\r");
+            DBG_PRINTF("CYBLE_EVT_GAPC_SCAN_START_STOP \r\n");
             DEBUG_BLE_PrintState();
             break;
             
         case CYBLE_EVT_GAP_SMP_NEGOTIATED_AUTH_INFO:
-            DBG_PRINTF("CYBLE_EVT_GAP_SMP_NEGOTIATED_AUTH_INFO \n\r");
+            DBG_PRINTF("CYBLE_EVT_GAP_SMP_NEGOTIATED_AUTH_INFO \r\n");
             break;
             
         case CYBLE_EVT_GAPC_SCAN_PROGRESS_RESULT:
-            //DBG_PRINTF("CYBLE_EVT_GAPC_SCAN_PROGRESS_RESULT \n\r");
+            //DBG_PRINTF("CYBLE_EVT_GAPC_SCAN_PROGRESS_RESULT \r\n");
             break;
             
         case CYBLE_EVT_GAP_AUTH_REQ:
-            DBG_PRINTF("CYBLE_EVT_GAP_AUTH_REQ \n\r");
+            DBG_PRINTF("CYBLE_EVT_GAP_AUTH_REQ \r\n");
             break;
 
         case CYBLE_EVT_GAP_PASSKEY_ENTRY_REQUEST:
-            DBG_PRINTF("CYBLE_EVT_GAP_PASSKEY_ENTRY_REQUEST \n\r");
+            DBG_PRINTF("CYBLE_EVT_GAP_PASSKEY_ENTRY_REQUEST \r\n");
             break;
 
         case CYBLE_EVT_GAP_PASSKEY_DISPLAY_REQUEST:
-            DBG_PRINTF("CYBLE_EVT_GAP_PASSKEY_DISPLAY_REQUEST: %6.6ld \n\r", *(uint32 *)eventParam);
+            DBG_PRINTF("CYBLE_EVT_GAP_PASSKEY_DISPLAY_REQUEST: %6.6ld \r\n", *(uint32 *)eventParam);
             break;
 
         case CYBLE_EVT_GAP_AUTH_FAILED:
@@ -192,33 +192,33 @@ void DEBUG_BLE_PrintAppEvent(uint32 event, void* eventParam)
             switch(*(CYBLE_GAP_AUTH_FAILED_REASON_T *)eventParam)
             {
                 case CYBLE_GAP_AUTH_ERROR_CONFIRM_VALUE_NOT_MATCH:
-                    DBG_PRINTF("CONFIRM_VALUE_NOT_MATCH\n\r");
+                    DBG_PRINTF("CONFIRM_VALUE_NOT_MATCH\r\n");
                     break;
                     
                 case CYBLE_GAP_AUTH_ERROR_INSUFFICIENT_ENCRYPTION_KEY_SIZE:
-                    DBG_PRINTF("INSUFFICIENT_ENCRYPTION_KEY_SIZE\n\r");
+                    DBG_PRINTF("INSUFFICIENT_ENCRYPTION_KEY_SIZE\r\n");
                     break;
                 
                 case CYBLE_GAP_AUTH_ERROR_UNSPECIFIED_REASON:
-                    DBG_PRINTF("UNSPECIFIED_REASON\n\r");
+                    DBG_PRINTF("UNSPECIFIED_REASON\r\n");
                     break;
                     
                 case CYBLE_GAP_AUTH_ERROR_AUTHENTICATION_TIMEOUT:
-                    DBG_PRINTF("AUTHENTICATION_TIMEOUT\n\r");
+                    DBG_PRINTF("AUTHENTICATION_TIMEOUT\r\n");
                     break;
                     
                 default:
-                    DBG_PRINTF("0x%x \n\r", *(CYBLE_GAP_AUTH_FAILED_REASON_T *)eventParam);
+                    DBG_PRINTF("0x%x \r\n", *(CYBLE_GAP_AUTH_FAILED_REASON_T *)eventParam);
                     break;
             }
             break;
 
         case CYBLE_EVT_GAP_DEVICE_CONNECTED:
-            DBG_PRINTF("CYBLE_EVT_GAP_DEVICE_CONNECTED: %x \n\r", cyBle_connHandle.bdHandle);
+            DBG_PRINTF("CYBLE_EVT_GAP_DEVICE_CONNECTED: %x \r\n", cyBle_connHandle.bdHandle);
             break;
 
         case CYBLE_EVT_GAP_DEVICE_DISCONNECTED:
-            DBG_PRINTF("CYBLE_EVT_GAP_DEVICE_DISCONNECTED, reason: %x \n\r", *(uint8*)eventParam);
+            DBG_PRINTF("CYBLE_EVT_GAP_DEVICE_DISCONNECTED, reason: %x \r\n", *(uint8*)eventParam);
             break;
 
         case CYBLE_EVT_GAP_AUTH_COMPLETE:
@@ -230,23 +230,23 @@ void DEBUG_BLE_PrintAppEvent(uint32 event, void* eventParam)
             break;
 
         case CYBLE_EVT_GAP_ENCRYPT_CHANGE:
-            DBG_PRINTF("CYBLE_EVT_GAP_ENCRYPT_CHANGE: %d \n\r", *(uint8 *)eventParam);
+            DBG_PRINTF("CYBLE_EVT_GAP_ENCRYPT_CHANGE: %d \r\n", *(uint8 *)eventParam);
             break;
 
         case CYBLE_EVT_GATT_CONNECT_IND:
-            DBG_PRINTF("CYBLE_EVT_GATT_CONNECT_IND \n\r");
+            DBG_PRINTF("CYBLE_EVT_GATT_CONNECT_IND \r\n");
             break;
             
         case CYBLE_EVT_GATT_DISCONNECT_IND:
-            DBG_PRINTF("CYBLE_EVT_GATT_DISCONNECT_IND \n\r");
+            DBG_PRINTF("CYBLE_EVT_GATT_DISCONNECT_IND \r\n");
             break;
             
         case CYBLE_EVT_GATTS_XCNHG_MTU_REQ:
-            DBG_PRINTF("CYBLE_EVT_GATTS_XCNHG_MTU_REQ \n\r");
+            DBG_PRINTF("CYBLE_EVT_GATTS_XCNHG_MTU_REQ \r\n");
             break;
             
         case CYBLE_EVT_GATTS_WRITE_REQ:
-            DBG_PRINTF("CYBLE_EVT_GATTS_WRITE_REQ: dataLen: %u\n\r", 
+            DBG_PRINTF("CYBLE_EVT_GATTS_WRITE_REQ: dataLen: %u\r\n", 
                         ((CYBLE_GATTS_WRITE_REQ_PARAM_T *)eventParam)->
                         handleValPair.value.len);
             
@@ -255,26 +255,26 @@ void DEBUG_BLE_PrintAppEvent(uint32 event, void* eventParam)
                                     handleValPair.value.val, 
                                 ((CYBLE_GATTS_WRITE_REQ_PARAM_T *)eventParam)->
                                     handleValPair.value.len);
-            DBG_PRINTF("\n\r");
+            DBG_PRINTF("\r\n");
             
-            DBG_PRINTF("attrHandle: %04x\n\r", 
+            DBG_PRINTF("attrHandle: %04x\r\n", 
                                 ((CYBLE_GATTS_WRITE_REQ_PARAM_T *)eventParam)->
                                     handleValPair.attrHandle);
             break;
             
         case CYBLE_EVT_GATTS_WRITE_CMD_REQ:
-            DBG_PRINTF("CYBLE_EVT_GATTS_WRITE_CMD_REQ \n\r");
+            DBG_PRINTF("CYBLE_EVT_GATTS_WRITE_CMD_REQ \r\n");
             DBG_PRINTF("data: ");
             DEBUG_PrintByteArr(((CYBLE_GATTS_WRITE_CMD_REQ_PARAM_T *)eventParam)-> 
                                     handleValPair.value.val, 
                                 ((CYBLE_GATTS_WRITE_CMD_REQ_PARAM_T *)eventParam)->
                                     handleValPair.value.len);
-            DBG_PRINTF("\n\r");
+            DBG_PRINTF("\r\n");
             break;
             
         case CYBLE_EVT_GATTS_READ_CHAR_VAL_ACCESS_REQ:
-            DBG_PRINTF("CYBLE_EVT_GATTS_READ_CHAR_VAL_ACCESS_REQ \n\r");
-            DBG_PRINTF("attrHandle: %04x\n\r", 
+            DBG_PRINTF("CYBLE_EVT_GATTS_READ_CHAR_VAL_ACCESS_REQ \r\n");
+            DBG_PRINTF("attrHandle: %04x\r\n", 
                                 ((CYBLE_GATTS_CHAR_VAL_READ_REQ_T *)eventParam)->
                                     attrHandle);
             
@@ -300,28 +300,28 @@ void DEBUG_BLE_PrintAppEvent(uint32 event, void* eventParam)
 
 void DEBUG_BLE_PrintScanProgress(CYBLE_GAPC_ADV_REPORT_T* eventParam)
 {
-    DBG_PRINTF("Scan result: \n\r");
+    DBG_PRINTF("Scan result: \r\n");
     DBG_PRINTF("event type:");
     switch (eventParam->eventType)
     {
         case CYBLE_GAPC_CONN_UNDIRECTED_ADV:
-            DBG_PRINTF("CONN_UNDIRECTED_ADV\n\r");
+            DBG_PRINTF("CONN_UNDIRECTED_ADV\r\n");
             break;
             
         case CYBLE_GAPC_CONN_DIRECTED_ADV:
-            DBG_PRINTF("CONN_DIRECTED_ADV\n\r");
+            DBG_PRINTF("CONN_DIRECTED_ADV\r\n");
             break;
             
         case CYBLE_GAPC_SCAN_UNDIRECTED_ADV:
-            DBG_PRINTF("DIRECTED_ADV\n\r");
+            DBG_PRINTF("DIRECTED_ADV\r\n");
             break;
             
         case CYBLE_GAPC_NON_CONN_UNDIRECTED_ADV:
-            DBG_PRINTF("NON_CONN_UNDIRECTED_ADV\n\r");
+            DBG_PRINTF("NON_CONN_UNDIRECTED_ADV\r\n");
             break;
             
         case CYBLE_GAPC_SCAN_RSP:
-            DBG_PRINTF("SCAN_RSP\n\r");
+            DBG_PRINTF("SCAN_RSP\r\n");
             break;
     }
     
@@ -329,10 +329,10 @@ void DEBUG_BLE_PrintScanProgress(CYBLE_GAPC_ADV_REPORT_T* eventParam)
     {      
         DBG_PRINTF("ADV data: ");
         DEBUG_PrintByteArr(eventParam->data, eventParam->dataLen);
-        DBG_PRINTF("\n\r");
+        DBG_PRINTF("\r\n");
     }
     
-    DBG_PRINTF("dataLen: %i \n\r", eventParam->dataLen);
+    DBG_PRINTF("dataLen: %i \r\n", eventParam->dataLen);
 
 }
 
@@ -341,8 +341,8 @@ void DEBUG_DISPLAY_Print(BLE_advPacketData_t *data)
 {
     char tempBuff[BLE_ADV_PACKET_TEXTLEN+1];
     
-    DBG_PRINTF("Try number: %u\n\r", data->TryNum);
-    DBG_PRINTF("Skier num: %u\n\r", data->SkierNum);
+    DBG_PRINTF("Try number: %u\r\n", data->TryNum);
+    DBG_PRINTF("Skier num: %u\r\n", data->SkierNum);
     DBG_PRINTF("Status: %x, Skier ", data->StatusByte);
     switch((data->StatusByte)&STATUS_SK_MASK)
     {
@@ -363,29 +363,29 @@ void DEBUG_DISPLAY_Print(BLE_advPacketData_t *data)
             break;
     }
     
-    DBG_PRINTF("\n\r");
+    DBG_PRINTF("\r\n");
     
-    DBG_PRINTF("Time start: %02x:%02x:%02x.%02x\n\r", 
+    DBG_PRINTF("Time start: %02x:%02x:%02x.%02x\r\n", 
                 (uint8)((data->TimeStart>>24)&0xFF),
                 (uint8)((data->TimeStart>>16)&0xFF),
                 (uint8)((data->TimeStart>>8)&0xFF),
                 (uint8)((data->TimeStart   )&0xFF));
     
-    DBG_PRINTF("Time finish: %02x:%02x:%02x.%02x\n\r", 
+    DBG_PRINTF("Time finish: %02x:%02x:%02x.%02x\r\n", 
                 (uint8)((data->TimeFinish>>24)&0xFF),
                 (uint8)((data->TimeFinish>>16)&0xFF),
                 (uint8)((data->TimeFinish>>8)&0xFF),
                 (uint8)((data->TimeFinish   )&0xFF));
     
-    DBG_PRINTF("Time result: %02x:%02x:%02x.%02x\n\r", 
+    DBG_PRINTF("Time result: %02x:%02x:%02x.%02x\r\n", 
                 (uint8)((data->TimeResult>>24)&0xFF),
                 (uint8)((data->TimeResult>>16)&0xFF),
                 (uint8)((data->TimeResult>>8)&0xFF),
                 (uint8)((data->TimeResult   )&0xFF));
     
     strncpy(tempBuff, data->Text, BLE_ADV_PACKET_TEXTLEN+1);
-    DBG_PRINTF("Text: %s\n\r", tempBuff);
-    DBG_PRINTF("\n\r");
+    DBG_PRINTF("Text: %s\r\n", tempBuff);
+    DBG_PRINTF("\r\n");
     
 }
 
