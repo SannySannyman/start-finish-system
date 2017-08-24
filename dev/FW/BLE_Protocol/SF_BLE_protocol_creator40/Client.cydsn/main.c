eaece78e1038d6174ca536d291171d4fae0c6934
Oleksandr Podolchak <ol.podolchak@gmail.com>
2017-08-24 15:45:23 +0300

    Оновлено головну функцію для роботи з новим функціоналом

--- dev/FW/BLE_Protocol/SF_BLE_protocol_creator40/Client.cydsn/main.c
+++ dev/FW/BLE_Protocol/SF_BLE_protocol_creator40/Client.cydsn/main.c
@@ -21,46 +21,23 @@
 volatile uint8 displayNextFlag = 0;
 
 
-CY_ISR(isr_DisplayNext_Handler)
-{
-    displayNextFlag = 1;
-    Timer_Display_ClearInterrupt(Timer_Display_INTR_MASK_TC);
-}
-
 int main(void)
-{
-    uint8 num = 0;
-    BLE_advPacketData_t skierData;
-    
+{    
     CyGlobalIntEnable; /* Enable global interrupts. */
     
     DEBUG_Init();
     BLE_Init();
     LEDD_Init();
-    Timer_Display_Start();
-    isr_DisplayNext_StartEx(isr_DisplayNext_Handler);
+    DisplayData_Init();
     
     LEDD_GotoXY(0, 0);
     LEDD_Str("SF System v1.0");
+    DisplayData_StartShow(0, SHOWMODE_BLINK);
 
     for(;;)
     {
         CyBle_ProcessEvents();
-        if(displayNextFlag == 1)
-        {
-            displayNextFlag = 0;
-            
-            if(BLE_ADV_RESULT_OK == BLE_ADV_DataBuff_DataGet(&skierData, num))
-            {
-                DisplayData_ShowSkierData(&skierData);
-            }
-            
-            num++;
-            if(num >= BLE_ADV_DataBuff_Size())
-            {
-                num = 0;
-            }
-        }
+        DisplayData_Process();
     }
 }
 
